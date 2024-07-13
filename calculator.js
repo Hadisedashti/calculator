da6b3688e0445ccd1154bc5a9d8a951d9250d134
hadise <dashtihadise@gmail.com>
2024-07-13 23:38:38 +0330

    1403.04.23

--- calculator.js
+++ calculator.js
@@ -6,3 +6,7 @@ function mosavi (){
     let num2 = +result.slice(operator,count)
     document.getElementById("text").value = num1 * num2
 }
+function mosavi (){
+    let result = document.getElementById("text").value
+    let count = result.length
+}
