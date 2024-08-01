cba219c9edefa95d76842d83a3aa626b44e55793
hadise <dashtihadise@gmail.com>
2024-08-01 18:01:44 +0330

    1403.05.11

--- src/calculator.js
+++ src/calculator.js
@@ -1,13 +1,46 @@
 function mosavi (){
     let result = document.getElementById("text").value
     let count = result.length
-    let operator = result.indexOf("*")+1
+    let operator = result.indexOf("+")+1
     let num1 = +result.slice (0,operator-1)
     let num2 = +result.slice(operator,count)
-    document.getElementById("text").value = num1 * num2
+    document.getElementById("text").value = num1 + num2
 }
-function mosavi (){
-    let result = document.getElementById("text").value
-    let count = result.length
-    let operator =result.indexOf("*")+1
+
+function sum(){
+    let num1= Number(document.getElementById("num1").value)
+    let num2 = Number(document.getElementById("ntm2".value))
+    document.getElementById("result").textContent = num1 + num2
+}
+function tafriq(){
+    let num1= Number(document.getElementById("num1").value)
+    let num2 = Number(document.getElementById("ntm2".value))
+    document.getElementById("result").textContent = num1 - num2
+}
+function zarb(){
+    let num1= Number(document.getElementById("num1").value)
+    let num2 = Number(document.getElementById("ntm2".value))
+    document.getElementById("result").textContent = num1 * num2
+}
+function taqsim(){
+    let num1= Number(document.getElementById("num1").value)
+    let num2 = Number(document.getElementById("ntm2".value))
+    document.getElementById("result").textContent = num1 / num2
+}
+
+switch(operator){
+    case"+":
+     sum()
+     break;
+    case"-":
+     tafriq()
+     break;
+    case"*":
+     zarb()
+     break;
+    case"/":
+     taqsim()
+     break;
+    defult:
+    alert ("لطفا مقدار صحیح وارد کنید") 
 }
